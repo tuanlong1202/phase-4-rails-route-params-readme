d852fdc7fbf51ba1c5b654133cee5516bc6e0692
tuanlong1202 <tuanlong1202@gmail.com>
2021-08-28 22:31:14 -0400

    Phase 4 rails route params readme

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # model
+    cheese = Cheese.find_by(id: params[:id])
+    # view
+    render json:cheese
+  end
+
 end
